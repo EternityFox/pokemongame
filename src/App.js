e6f2ea914c66d8fcd402f05622705b936c95e9b6
EternityFox <77994943+EternityFox@users.noreply.github.com>
2021-01-30 16:32:59 +1000

    Howework-3
    Справился с map

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
     > 
       <div className="flex">
         {
-          POKEMONS.map(item => <PokemonCard {...item}/>)
+         POKEMONS.map(item => <PokemonCard key={item.id} {...item}/>)
         }
       </div>     
     </Layout> 
